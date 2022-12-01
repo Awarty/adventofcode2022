f72fb6d78da8014d8f50c2f1939d24b64b567a9c
Jacob Almrot <jacob.almrot@gaia.se>
2022-12-01 20:59:04 +0100

    Removed target dir in rust.

--- 01/rust/target/debug/deps/rust-b169b32b63726de3.d
+++
@@ -1,5 +0,0 @@
-/home/jacob/dev/eget/adventofcode2022/01/rust/target/debug/deps/rust-b169b32b63726de3.rmeta: src/main.rs
-
-/home/jacob/dev/eget/adventofcode2022/01/rust/target/debug/deps/rust-b169b32b63726de3.d: src/main.rs
-
-src/main.rs:
--- 01/rust/target/debug/deps/rust-b441f7a068c97985.d
+++
@@ -1,5 +0,0 @@
-/home/jacob/dev/eget/adventofcode2022/01/rust/target/debug/deps/rust-b441f7a068c97985.rmeta: src/main.rs
-
-/home/jacob/dev/eget/adventofcode2022/01/rust/target/debug/deps/rust-b441f7a068c97985.d: src/main.rs
-
-src/main.rs:
